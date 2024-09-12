8953af390fb96fdf892ba94a8f1a223c8fb42667
Matipala <josue.molina@ucb.edu.bo>
2024-09-12 19:34:12 -0400

    update iformulario

--- ServicesApp/Services/IFormularioService.cs
+++ ServicesApp/Services/IFormularioService.cs
@@ -1,5 +1,5 @@
 public interface IFormularioService{
     public void enviarDocumento();
     public bool validarPostulacion();
-    public void GuardarDatos(Docente? nuevoDocente, List<Documento>? documentosObligatorios, List<Documento>? documentosOpcionales);
+    public void GuardarDatosFormularioDocente(Docente? nuevoDocente, List<Documento>? documentosObligatorios, List<Documento>? documentosOpcionales);
 }
