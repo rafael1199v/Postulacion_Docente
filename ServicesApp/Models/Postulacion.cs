bb3552821170080f370f56fb8d0d14d32f9a923c
Matipala <josue.molina@ucb.edu.bo>
2024-10-11 20:35:15 -0400

    primer parcial

--- ServicesApp/Models/Postulacion.cs
+++ ServicesApp/Models/Postulacion.cs
@@ -1,8 +1,8 @@
 public class Postulacion
 {
     public int PostulacionId { get; set; }
-    public List<(Docente, int)> notasDocente { get; set; } = null!;
+    public List<(Docente Docente, int Nota)> NotasDocente { get; set; } = new List<(Docente, int)>();
     public int MateriaId { get; set; }
     public int JefeCarreraId { get; set; }
-
+    public string? Estado { get; set; }
 }
