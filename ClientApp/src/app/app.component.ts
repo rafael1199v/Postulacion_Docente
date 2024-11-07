2f714947713d7a3ef359db1b0a9a8e43b11740b2
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2024-11-07 19:03:29 -0400

    FrontEnd Login

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { RouteToken } from './models/enums/route.enum';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  tokenRouter: RouteToken = RouteToken.Home;
   title = 'app';
+
+
+  constructor(private activadedRoute : ActivatedRoute, private router: Router){
+   
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        switch(e.url)
+        {
+          case'/login':
+            this.tokenRouter = RouteToken.Login;
+            break;
+          case '/register':
+            this.tokenRouter = RouteToken.Register;
+            break;
+          default:
+            this.tokenRouter = RouteToken.Home;
+            break;
+           
+        }
+        
+      }
+    });
+  }
+
+
 }
---
+++ ClientApp/src/app/models/enums/route.enum.ts
@@ -0,0 +1,7 @@
+export enum RouteToken
+{
+    Login,
+    Register,
+    Home,
+    //Si se necesita se agregan mas elementos al enum
+}
