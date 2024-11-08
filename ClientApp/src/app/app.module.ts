2c5fcddb45f09d7b002dc96f5639ef140276a826
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-11-07 20:15:27 -0400

    wtfffff
    se me olvidó que ya dejé de hacer lo debido en esta rama D:

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +28,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'footer', component: FooterComponent },
     ])
   ],
   providers: [],
---
+++ ServicesApp/Models/Curriculum.cs
@@ -0,0 +1,5 @@
+public class Curriculum{
+    public Docente? postulante {get;set;}
+    public List<Documento>? documentos {get;set;}
+
+}
