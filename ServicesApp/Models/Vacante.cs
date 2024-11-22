7ef3bcbabe2fc9f9521dd77c2229d4934ca168b3
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-11-22 09:44:41 -0400

    aaaaaaaaaaaaaa
    tenemos examen hoy xddddd

--- ServicesApp/Models/Vacante.cs
+++ ServicesApp/Models/Vacante.cs
@@ -3,6 +3,7 @@ public class Vacante
     public string? NombreVacante { get; set; } = string.Empty;
     public string? Materia { get; set; }
     public string? DescripcionVacante { get; set; } = string.Empty;
+    public List<Postulacion> Postulaciones {get;set;}
     public DateTime FechaInicion {get; set;}
     public DateTime FechaFinalizacion {get; set;}
 }
